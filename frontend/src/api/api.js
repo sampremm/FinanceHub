fa068d3580b60bbd0333011847b4b310d6396357
sampremm <samprem888111@gmail.com>
2026-04-06 20:35:20 +0530

    fixed api error

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const API = axios.create({
 baseURL: import.meta.env.VITE_API_URL,
 });
+console.log("API URL:", import.meta.env.VITE_API_URL);
 
 // Auth
 export const authAPI = {
